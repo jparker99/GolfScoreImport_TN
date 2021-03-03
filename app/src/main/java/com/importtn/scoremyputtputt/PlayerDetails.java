0abfc70b258c38f6f2832f3fabd0f40e631311dd
Jared Parker <jwp1299@gmail.com>
2021-03-03 13:53:12 -0500

    Finished core functionality

--- app/src/main/java/com/importtn/scoremyputtputt/PlayerDetails.java
+++ app/src/main/java/com/importtn/scoremyputtputt/PlayerDetails.java
@@ -1,15 +1,20 @@
 package com.importtn.scoremyputtputt;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PlayerDetails extends AppCompatActivity {
     Game gameObject;
     Button bgn;
+    Button incrementPlayers;
+    Button decrementPlayers;
+    EditText numberPlayers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,9 @@ public class PlayerDetails extends AppCompatActivity {
         setContentView(R.layout.layout_player_details);
 
         bgn = findViewById(R.id.placeHolderStGameButton);
+        incrementPlayers = findViewById(R.id.tempIncreasePlayers);
+        decrementPlayers = findViewById(R.id.tempReducePlayers);
+        numberPlayers = findViewById(R.id.tempNumberPlayers);
 
 
         Intent i = getIntent();
@@ -25,19 +33,49 @@ public class PlayerDetails extends AppCompatActivity {
 
         bgn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
-                finalizePlayers();
+                int number = Integer.parseInt(numberPlayers.getText().toString());
+                finalizePlayers(number);
+            }
+        });
+
+        incrementPlayers.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                incrementPlayers();
+            }
+        });
+        decrementPlayers.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                decrementPlayers();
             }
         });
     }
 
-    private void finalizePlayers(){
+    private void finalizePlayers(int number){
         //Placeholder method, will need to have player names and icons later.
-        gameObject.addPlayer(new Player("Player 1", ""));
-        gameObject.addPlayer(new Player("Player 2", ""));
-        gameObject.addPlayer(new Player("Player 3", ""));
-        gameObject.addPlayer(new Player("Player 4", ""));
+        for(int i = 1; i <= number; i++){
+            gameObject.addPlayer(new Player("Player " + i, ""));
+        }
+
         Intent i = new Intent(this, EnterScore.class);
         i.putExtra("gameObject", gameObject);
         startActivity(i);
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void incrementPlayers(){
+        int currPlayers = Integer.parseInt(numberPlayers.getText().toString());
+        currPlayers++;
+        numberPlayers.setText(Integer.toString(currPlayers));
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void decrementPlayers(){
+
+        int currPlayers = Integer.parseInt(numberPlayers.getText().toString());
+        if(currPlayers > 1){
+            currPlayers--;
+            numberPlayers.setText(Integer.toString(currPlayers));
+        }
+
     }
 }
