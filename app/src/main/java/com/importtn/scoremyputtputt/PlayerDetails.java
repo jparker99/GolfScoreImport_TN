d2f045ded21b06d5567522add484403996c684aa
Bufford99 <iambufford99@gmail.com>
2021-03-03 01:24:37 -0500

    wip: player details

--- app/src/main/java/com/importtn/scoremyputtputt/PlayerDetails.java
+++ app/src/main/java/com/importtn/scoremyputtputt/PlayerDetails.java
@@ -4,12 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerDetails extends AppCompatActivity {
     Game gameObject;
-    Button bgn;
+    ImageButton bgn;
+
+    List<Player> players = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +25,6 @@ public class PlayerDetails extends AppCompatActivity {
         setContentView(R.layout.layout_player_details);
 
         bgn = findViewById(R.id.placeHolderStGameButton);
-
 
         Intent i = getIntent();
         gameObject = (Game)i.getSerializableExtra("gameObject");
@@ -30,14 +36,26 @@ public class PlayerDetails extends AppCompatActivity {
         });
     }
 
+    private String retrieveName(int id) {
+        EditText editText = findViewById((id));
+
+        System.out.println(editText.getText().toString());
+
+        return editText.getText().toString();
+    }
+
     private void finalizePlayers(){
         //Placeholder method, will need to have player names and icons later.
-        gameObject.addPlayer(new Player("Player 1", ""));
-        gameObject.addPlayer(new Player("Player 2", ""));
-        gameObject.addPlayer(new Player("Player 3", ""));
-        gameObject.addPlayer(new Player("Player 4", ""));
+        players.add(new Player(retrieveName(R.id.player1Name), ""));
+        players.add(new Player(retrieveName(R.id.player2Name), ""));
+        players.add(new Player(retrieveName(R.id.player3Name), ""));
+        players.add(new Player(retrieveName(R.id.player4Name), ""));
+
+        gameObject.setPlayers(players);
+
         Intent i = new Intent(this, EnterScore.class);
         i.putExtra("gameObject", gameObject);
+        finishAffinity();
         startActivity(i);
     }
 }
