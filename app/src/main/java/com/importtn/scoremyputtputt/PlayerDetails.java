fbe43d36059ccbf4b7bb1c81ad5f50f6047655a2
udai <singhu@vcu.edu>
2021-03-04 14:15:48 -0500

    allows us to add infinitely more players to the game
    -needs to look prettier
    -need to acknowledge the players 1-4 blanks

--- app/src/main/java/com/importtn/scoremyputtputt/PlayerDetails.java
+++ app/src/main/java/com/importtn/scoremyputtputt/PlayerDetails.java
@@ -4,9 +4,12 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,8 +19,12 @@ import java.util.List;
 public class PlayerDetails extends AppCompatActivity {
     Game gameObject;
     ImageButton bgn;
-
+    private EditText mEditText;
+    private Button mButton;
+    private LinearLayout mLayout;
+    private int numPlayers = 0;
     List<Player> players = new ArrayList<>();
+    List<String> names = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +43,34 @@ public class PlayerDetails extends AppCompatActivity {
             }
         });
 
+        mLayout = (LinearLayout) findViewById(R.id.linearLayout);
+        mEditText = (EditText) findViewById(R.id.editText);
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(onClick());
+        TextView textView = new TextView(this);
+        textView.setText("New text");
+
+    }
+    private View.OnClickListener onClick() {
+        return new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mLayout.addView(createNewTextView(mEditText.getText().toString()));
+            }
+        };
+    }
+
+    private TextView createNewTextView(String text) {
+        final ViewGroup.LayoutParams lparams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        final TextView textView = new TextView(this);
+        textView.setLayoutParams(lparams);
+        textView.setText(text);
+        names.add(text);
+        numPlayers++;
+
+
+        return textView;
     }
 
     private String retrieveName(int id) {
@@ -52,6 +87,10 @@ public class PlayerDetails extends AppCompatActivity {
         players.add(new Player(retrieveName(R.id.player2Name), ""));
         players.add(new Player(retrieveName(R.id.player3Name), ""));
         players.add(new Player(retrieveName(R.id.player4Name), ""));
+
+        for(int i = 0; i<numPlayers; i++){
+            players.add(new Player(names.get(i), ""));
+        }
 
         gameObject.setPlayers(players);
 
