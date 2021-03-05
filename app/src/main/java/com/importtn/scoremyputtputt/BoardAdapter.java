eac482a2fe28c92689c12bc0541aad41775b0031
Bufford99 <iambufford99@gmail.com>
2021-03-05 16:36:07 -0500

    feat(endScreen): beautify

--- app/src/main/java/com/importtn/scoremyputtputt/BoardAdapter.java
+++ app/src/main/java/com/importtn/scoremyputtputt/BoardAdapter.java
@@ -1,19 +1,28 @@
 package com.importtn.scoremyputtputt;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Color;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.Collections;
 import java.util.List;
 
+import static android.app.PendingIntent.getActivity;
+import static java.security.AccessController.getContext;
+
 public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.ViewHolder> {
     private final List<Player> players;
+    Context context;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private final TextView playerNameThing;
@@ -21,10 +30,21 @@ public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.ViewHolder> 
 
         public ViewHolder(View view) {
             super(view);
-            playerNameThing = (TextView) view.findViewById(R.id.playerNameThing);
-            playerScoreThing = (TextView) view.findViewById(R.id.playerScoreThing);
-        }
 
+            playerNameThing = (TextView) view.findViewById(R.id.playerNameThing);
+            playerNameThing.setTextColor(Color.BLACK);
+            playerNameThing.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+            playerNameThing.setGravity(Gravity.CENTER);
+            playerNameThing.setPadding(8, 24, 8, 24);
+            playerNameThing.setBackgroundResource(R.drawable.customborder);
+
+            playerScoreThing = (TextView) view.findViewById(R.id.playerScoreThing);
+            playerScoreThing.setTextColor(Color.BLACK);
+            playerScoreThing.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+            playerScoreThing.setGravity(Gravity.CENTER);
+            playerScoreThing.setPadding(8, 24, 8, 24);
+            playerScoreThing.setBackgroundResource(R.drawable.customborder);
+        }
 
         public TextView getPlayerNameThing() {
             return playerNameThing;
@@ -35,7 +55,8 @@ public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.ViewHolder> 
         }
     }
 
-    public BoardAdapter(Game gameObject) {
+    public BoardAdapter(Game gameObject, Context context) {
+        this.context = context;
         this.players = gameObject.getPlayers();
         Collections.sort(players);
     }
--- app/src/main/java/com/importtn/scoremyputtputt/EndScreen.java
+++ app/src/main/java/com/importtn/scoremyputtputt/EndScreen.java
@@ -1,13 +1,19 @@
 package com.importtn.scoremyputtputt;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Collections;
 
 public class EndScreen extends AppCompatActivity {
     Game gameObject;
@@ -15,6 +21,7 @@ public class EndScreen extends AppCompatActivity {
     RecyclerView mRecyclerView;
     BoardAdapter mAdapter;
     Button finishButton;
+    TextView playerWinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +37,18 @@ public class EndScreen extends AppCompatActivity {
             }
         });
 
+        playerWinner = (TextView) findViewById(R.id.player_winner);
+        String winnerText = "✨ " + gameObject.getWinner().getName() + " wins the game! ✨";
+        playerWinner.setText(winnerText);
+        playerWinner.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 24);
+        playerWinner.setTextColor(Color.BLACK);
+        playerWinner.setGravity(Gravity.CENTER);
+        playerWinner.setPadding(0, 64, 0, 128);
+
         mRecyclerView = (RecyclerView) findViewById(R.id.listPlayerObjects);
-        mAdapter = new BoardAdapter(gameObject);
+        mAdapter = new BoardAdapter(gameObject, getApplicationContext());
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-
-
     }
 
     private void finishUp(){
